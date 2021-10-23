672426700775b73af719a0f4e842e105fc519fb1
EldarOru <macdog108@gmail.com>
2021-10-23 22:40:33 +0300

    start

---
+++ app/src/main/java/com/example/eventracker/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.eventracker.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.eventracker.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
